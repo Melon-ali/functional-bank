d8a8b730dbf1c373595bf75225e41d0e51720820
Melon-alie <rubel300ali@gmail.comm>
2021-08-18 01:40:23 +0600

    previous total added

--- js/banking2.js
+++ js/banking2.js
@@ -2,7 +2,7 @@ const depositBtn = document.getElementById('deposit-button')
 const withdrawBtn = document.getElementById('withdraw-button')
 
 depositBtn.addEventListener('click', function(){
-    const depositTotal = document.getElementById('deposit-total')
+    /* const depositTotal = document.getElementById('deposit-total')
     const depositTotalText = depositTotal.innerText
     const depositTotalNumber = parseFloat(depositTotalText)
     const newDeposit = document.getElementById('deposit-input')
@@ -16,12 +16,14 @@ depositBtn.addEventListener('click', function(){
     const balanceTotaltext =balanceTotal.innerText
     const balanceTotalNumber = parseFloat(balanceTotaltext)
 
-    balanceTotal.innerText = balanceTotalNumber + newDepositNumber
+    balanceTotal.innerText = balanceTotalNumber + newDepositNumber */
+
+    changeBalance("deposit")
 
 })
 
 withdrawBtn.addEventListener('click', function(){
-    const withdrawTotal = document.getElementById('withdraw-total')
+    /* const withdrawTotal = document.getElementById('withdraw-total')
     const withdrawTotalText = withdrawTotal.innerText
     const withdrawTotalNumber = parseFloat(withdrawTotalText)
 
@@ -36,5 +38,37 @@ withdrawBtn.addEventListener('click', function(){
     const balanceTotaltext =balanceTotal.innerText
     const balanceTotalNumber = parseFloat(balanceTotaltext)
 
-    balanceTotal.innerText = balanceTotalNumber - newWithdrawNumber
+    balanceTotal.innerText = balanceTotalNumber - newWithdrawNumber */
+
+    changeBalance("withdraw")
 })
+
+function changeBalance(type){
+    const previousTotal = document.getElementById(`${type}-total`)
+    const previousTotalText = previousTotal.innerText
+    const previousTotalNumber = parseFloat(previousTotalText)
+    const newMoney = document.getElementById(`${type}-input`)
+    const newMoneyText = newMoney.value
+    if(newMoneyText.length != 0 ) {
+        const newMoneyNumber = parseFloat(newMoneyText)
+        previousTotal.innerText = previousTotalNumber + newMoneyNumber
+        newMoney.value = ''
+
+
+        const balanceTotal = document.getElementById('balance-total')
+        const balanceTotaltext =balanceTotal.innerText
+        const balanceTotalNumber = parseFloat(balanceTotaltext)
+
+        balanceTotal.innerText = balanceTotalNumber + newMoneyNumber
+
+        if(type == 'withdraw'){
+            balanceTotal.innerText = balanceTotalNumber - newMoneyNumber
+        }
+        else{
+            balanceTotal.innerText = balanceTotalNumber + newMoneyNumber
+        }
+    }
+    else{
+        alert('Please Enter a Number')
+    }
+}
